f91593bcbff504a90aecb0ecd92c713b5a922c38
morisum <moris@r.sannet.ne.jp>
2025-03-06 07:24:20 +0900

    カメラ起動のボタンまで

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var captureImage: UIImage? = nil
+    @State var isShowSheet: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            if let captureImage {
+                Image(uiImage: captureImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            Spacer()
+            Button {
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("カメラは利用できます")
+                    isShowSheet.toggle()
+                } else {
+                    print( "カメラは利用できません")
+                }
+            } label: {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(isShowSheet: $isShowSheet, capturedImage: $captureImage)
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
